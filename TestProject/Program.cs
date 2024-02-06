1e7ddabe3d9bd1c0b1531c46ee02febaa06c41fb
s11246584 <s11246584@students.duvalschools.org>
2024-02-06 14:09:45 -0800

    rrf

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        int myInt = 32
+        float myFloat = 2.43F;
+        bool myBool = true;
+        string myString = "Hiya.\n";
+
+        Console.WriteLine("My Integer variable is " + myInt);
+        Console.WriteLine("My Float variable is " + myFloat);
+        Console.WriteLine("My Bool variable is " + myBool);
+        Console.WriteLine("My String variable is " + myString);
+
+        for (int i = 0; i<= 20; i++) {
+            Console.WriteLine("I ate " + i + " slices of pizza for lunch.\n");
+        }
     }
 }
